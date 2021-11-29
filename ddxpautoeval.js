821928da0905a7dfb41a4c02d4783c69309fc63d
justplayscript <90706144+justplayscript@users.noreply.github.com>
2021-11-30 01:12:14 +0800

    兼容青龙推送

--- ddxpautoeval.js
+++ ddxpautoeval.js
@@ -5,6 +5,8 @@
 */
 
 const $ = new Env('叮咚自动喂鱼浇水');
+const notify = $.isNode() ? require('./sendNotify') : null;
+
 const dr = "@"
 let ddxpurlArr = [],
     ddxphdArr = [],
@@ -23,7 +25,7 @@ let ddxpjs = $.getdata('ddxpjs')
         if (ddxphdArr[i]) {
             getUrl(ddxpurlArr[i])
             ddxphd = ddxphdArr[i];
-
+			
             $.index = i + 1;
             console.log(`\n开始【叮咚浇水${$.index}】`)
             await ddxTaskLog();
@@ -132,8 +134,8 @@ function ddxpyt(timeout = 0) {
                             console.log(`\n鱼儿已完成: ${result.data.seed.expPercent}%`)
                             if (result.data.seed.expPercent >= 100) {
                                 console.log(`\n鱼儿已养成,请及时收取`)
-								 $.msg($.name, "", `\n恭喜你,鱼儿已养成,请及时收取`)
-                                // $.msg()
+								if (notify){await notify.sendNotify($.name, `\n恭喜你,鱼儿已养成,请及时收取`);}
+								else $.msg($.name, "", `\n恭喜你,鱼儿已养成,请及时收取`)
                             } else if (result.data.feed.amount >= 10) {
                                 console.log(`鱼饵数量${result.data.feed.amount}`)
                                 await $.wait(1000);
@@ -282,8 +284,8 @@ function ddxpgyjs(timeout = 0) {
 
                             if (result.data.seed.expPercent >= 100) {
                                 console.log(`\n水果已种成,请及时收取`)
-								 $.msg($.name, "", `\n恭喜你,水果已种成,请及时收取`)
-                                // $.msg()
+								if (notify){await notify.sendNotify($.name, `\n恭喜你,鱼儿已养成,请及时收取`);}
+								else $.msg($.name, "", `\n恭喜你,水果已种成,请及时收取`)
                             } else if (result.data.feed.amount >= 10) {
                                 console.log(`水滴数量${result.data.feed.amount}`)
                                 await $.wait(1000);
